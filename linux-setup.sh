aaa64267ac869583f0f375ecc857028bf6ef6049
Daron McIntosh <mcintoshdaron@gmail.com>
2023-01-12 10:37:17 -0800

    changes

--- linux-setup.sh
+++ linux-setup.sh
@@ -26,7 +26,7 @@ install_ohmyzsh() {
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   # git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-  sudo chsh -s $(which zsh)
+  chsh -s $(which zsh)
 }
 
 setup_dotfiles() {
