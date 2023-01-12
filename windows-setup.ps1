1b5259c728e273dc4d40ae1c3239e1660ded8219
Daron McIntosh <mcintoshdaron@gmail.com>
2023-01-12 11:47:42 -0800

    changes

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -5,11 +5,18 @@
 if (!(Test-Path -Path $PROFILE)) {
   New-Item -ItemType File -Path $PROFILE -Force
 }
+# default is restricted which prevents $PROFILE from running
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 # TODO: download file from github then import then delete
 # rancher desktop is a bit buggy at the moment so we'll stick with docker desktop for now
 # winget import --import-file winstall.json
+winget install -e --id Google.Chrome
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id JanDeDobbeleer.OhMyPosh
+
+# TODO: add following to $PROFILE
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_lean.omp.json" | Invoke-Expression
 
 # install font
 Invoke-WebRequest -URI "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/FiraCode.zip" -Out FiraCode.zip
