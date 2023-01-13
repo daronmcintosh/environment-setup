036a5ee817e0cb30436ca758cc083f5840581f8e
Daron McIntosh <mcintoshdaron@gmail.com>
2023-01-13 09:09:34 -0800

    changes

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # environment-setup
 
+<!-- TODO: both setups should be idempotent -->
 ## linux
 
 ```sh
@@ -21,13 +22,4 @@ docker build --progress=plain -t foo . && docker run --rm -it foo
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/daronmcintosh/environment-setup/main/windows-setup.ps1'))
 ```
 
-### test with windows sandbox
-
-install winget in sandbox (https://learn.microsoft.com/en-us/windows/package-manager/winget/#install-winget-on-windows-sandbox):
-
-```ps1
-Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.3.2691/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile .\MicrosoftDesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage MicrosoftDesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-```
+### test with windows 11 dev environment
--- windows-setup.ps1
+++ windows-setup.ps1
@@ -1,31 +1,73 @@
-# TODO: fail if not admin?
-# winget comes bundled in modern versions of windows: https://learn.microsoft.com/en-us/windows/package-manager/winget/
+function Update-Profile {
+  Write-Output "updating profile"
+  # create powershell profile if not exists: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.3
+  if (!(Test-Path -Path $PROFILE)) {
+    New-Item -ItemType File -Path $PROFILE -Force
+  }
+  Install-Module -Name PowerShellGet -Force
+  Install-Module -Name Terminal-Icons -Repository PSGallery
+  Start-Process -Verb RunAs powershell.exe "Install-Module PSReadLine -AllowPrerelease -Force"
 
-# create powershell profile if not exists: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.3
-if (!(Test-Path -Path $PROFILE)) {
-  New-Item -ItemType File -Path $PROFILE -Force
+  @"
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_lean.omp.json" | Invoke-Expression
+Import-Module -Name Terminal-Icons
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+"@ >> $PROFILE
 }
 
-# TODO: download file from github then import then delete
-# rancher desktop is a bit buggy at the moment so we'll stick with docker desktop for now
-# winget import --import-file winstall.json
-# winget install -e --id Google.Chrome
-winget install -e --id Microsoft.WindowsTerminal
-winget install -e --id JanDeDobbeleer.OhMyPosh
+function Install-Fonts {
+  Write-Output "installing fonts"
 
-# TODO: add following to $PROFILE
-# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_lean.omp.json" | Invoke-Expression
+  Invoke-WebRequest -URI "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/FiraCode.zip" -Out FiraCode.zip
+  mkdir ~/Downloads/Fonts
+  Expand-Archive FiraCode.zip -DestinationPath ~/Downloads/Fonts -Force
 
-# install font
-Invoke-WebRequest -URI "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/FiraCode.zip" -Out FiraCode.zip
-Expand-Archive FiraCode.zip -DestinationPath C:\Windows\fonts -Force
+  $fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+  foreach ($file in Get-ChildItem ~/Downloads/Fonts/*.ttf) {
+    $fileName = $file.Name
+    if (-not(Test-Path -Path "C:\Windows\fonts\$fileName" )) {
+      Write-Output $fileName
+      Get-ChildItem $file | ForEach-Object { $fonts.CopyHere($_.fullname) }
+    }
+  }
+  Copy-Item ~/Downloads/Fonts/*.ttf c:\windows\fonts\
+}
 
-# TODO: should the $PROFILE for powershell file be tracked in git? for loading oh my posh
-# TODO: should profile config for windows terminal also be tracked? for setting default font
+function Install-Packages {
+  Write-Output "installing packages"
+  # winget comes bundled in modern versions of windows: https://learn.microsoft.com/en-us/windows/package-manager/winget/
+  # TODO: download file from github then import then delete
+  # rancher desktop is a bit buggy at the moment so we'll stick with docker desktop for now
+  # winget import --import-file winstall.json
+  # winget install -e --id Google.Chrome
+  # winget install -e --id Microsoft.WindowsTerminal
+  winget install -e --id JanDeDobbeleer.OhMyPosh
+  winget install -e --id Microsoft.VisualStudioCode
+}
 
+function Install-WSL {
+  Write-Output "installing wsl"
+  # install wsl
+  # wsl --install
 
-# install wsl
-# wsl --install
+  # install distro from tar file. distro has default user, daron, and linux-setup.sh was already ran in it
+  # wsl --import BaseUbuntu BaseUbuntu .\ubuntu-22.04.tar
 
-# install distro from tar file. distro has default user, daron, and linux-setup.sh was already ran in it
-# wsl --import BaseUbuntu BaseUbuntu .\ubuntu-22.04.tar
+}
+
+function Main {
+  # run script as administrator
+  if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit
+  }
+
+  Update-Profile
+  Install-Fonts
+  # Install-Packages
+  # Install-WSL
+}
+
+Main
